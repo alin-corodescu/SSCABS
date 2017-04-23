e1b1e83fe1cd8d53bd01783a7ab6f45f432adf09
alin-corodescu <alin.corodescu@gmail.com>
2017-04-23 22:47:21 +0100

    works with new cipher files

--- src/blackBoard/BlackboardMain.java
+++ src/blackBoard/BlackboardMain.java
@@ -53,6 +53,15 @@ public class BlackboardMain {
         	// example decryption for your instruction
 		    String message = "\"Wifv cuixb’g mihgm, cht,\" mub Mbdhx ghyz.\"Gi ym yg,\" Hvmfvi ghyz.\"Afbgg wif’z ebmmbv xim xbbz kb mubx.\"Hzh’g cuixb ohg xim mihgm.Yx mub qhv, ub ptyccbz ym icbx hxz guiobz ym uyg ehzab mubx ohymbz h kikbxm ouytb ym nbvypybz uyg yzbxmymw oymu mub Giqyht Uhvkixw evhyxg.Ixqb ym uhz, ym gcyttbz ymg afmg.Gub’z qhttbz mub thgm BdqfgbQtfe xfkebv h kixmu ebpivb hxz ub’z uhz ym zygqixxbqmbz.H obbs thmbv, gub ohg qhttyxa mub xbo xfkebv, moyqb kivb ebpivb ub qhfaum ubv.Gikboubvb yx muhm obbs, gub’z khzb qixmhqm oymu gikbixb oui’z aynbx ubv mub xbo xfkebv.Ym qiftz uhnb ebbx h pvybxz hm gquiit" +
 					" mitz ubv phqb-mi-phqb, efm yp ub ohg tfqsw, ym ohg ew cuixb.";
+
+            message = "Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia " +
+                    "esmm rwctjsxsza sj wmpramh, lxo txmarr jia aqsoaxwa sr pqaceiamn" +
+                    "sxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr " +
+                    "pyyacao rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxw" +
+                    "a jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia rmsuijarj aqso" +
+                    "axwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ct" +
+                    "rramm";
+
 			TextObject cipher = (TextObject) blackboard.layer("cipherText");
 			message = addSpacesAfterSymbols(message);
 			cipher.add(message);
